7fcc949307308d9da377f8ab0726ff4db4255e9f
nikololiahim <olomishcak@outlook.com>
2022-04-28 11:12:42 +0300

    fixed imports, removed Queries

--- backend/src/main/scala/trash/repository/postgres/BackendQueries.scala
+++ backend/src/main/scala/trash/repository/postgres/BackendQueries.scala
@@ -1,19 +1,12 @@
 package trash.utils.postgres
 
-import com.bot4s.telegram.models.Message
-import cats.effect.kernel.MonadCancelThrow
+import cats.effect.MonadCancelThrow
 import doobie.util.transactor.Transactor
-import doobie._
 import doobie.implicits._
-import doobie.postgres.implicits._
-import cats.syntax.all._
-import trash.repository.models.{DBMessage, Queries}
-import trash.repository.models.Queries._
+import trash.repository.models.DBMessage
 
-class BackendQueries[F[_]: MonadCancelThrow](
-                                           xa: Transactor[F]
-                                         ) extends Queries[F](xa){
-  def availableChatIds(): F[List[Long]] =
+class BackendQueries[F[_]](xa: Transactor[F])(implicit F: MonadCancelThrow[F]) {
+  def availableChatIds: F[List[Long]] =
     sql"""
       SELECT DISTINCT chat_id FROM message
        """.query[Long].to[List].transact(xa)
